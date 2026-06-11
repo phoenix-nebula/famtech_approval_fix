d548fdc4777a60f8d07a366a33a46d907b6a3d9b
Patrick Crisostomo <kylecode7213@gmail.com>
2026-06-11 16:21:07 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Famtech Approval Fix',
+    'version': '18.0.1.0.0',
+    'category': 'Approvals',
+    'summary': 'Fixes Attach Document error in Approvals app',
+    'depends': ['approvals'],
+    'author': 'Famtech Innovative',
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
