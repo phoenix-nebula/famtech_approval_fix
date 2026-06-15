2239bdf44ed8fed8904fb64e7d5eec35053326cb
Patrick Crisostomo <kylecode7213@gmail.com>
2026-06-15 14:20:42 +0800

    Update approvals_request.py

--- models/approvals_request.py
+++ models/approvals_request.py
@@ -3,11 +3,14 @@ from odoo import models
 class ApprovalRequest(models.Model):
     _inherit = 'approval.request'
 
-    def message_post(self, **kwargs):
-        # Fix: filter out None values from attachment_ids
-        # caused by a bug in Odoo 18 core Approvals app
-        if 'attachment_ids' in kwargs:
+    def message_post(self, *args, **kwargs):
+        # Clean attachment_ids from kwargs
+        if kwargs.get('attachment_ids'):
             kwargs['attachment_ids'] = [
-                a for a in kwargs['attachment_ids'] if a is not None
+                a for a in kwargs['attachment_ids']
+                if a is not None and a is not False
             ]
-        return super().message_post(**kwargs)
+            if not kwargs['attachment_ids']:
+                kwargs.pop('attachment_ids')
+
+        return super().message_post(*args, **kwargs)
